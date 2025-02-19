ca557913cacb0653d68d260b968cbba20c676022
Roni Sarkar <ronichandrasarkar@gmail.com>
2025-02-19 16:01:28 +0530

    delete app.css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import HeroSectionComponent from "./components/hero/hero_section.component";
 import HomeComponent from "./components/home/home.component";
 import LoginComponent from "./components/login/login.component";
