10da955ad37a6e1a787407dea27cb5c3ec93816f
Roni Sarkar <ronichandrasarkar@gmail.com>
2025-03-21 12:26:10 +0530

    changes

--- src/components/explore/explore.component.tsx
+++ src/components/explore/explore.component.tsx
@@ -1,18 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import BackHomeComponent from "../back_home/back.home.component";
 import ExploreViewListComponent from "./explore.view.list.component";
 import ExploreFeatureComponent from "./explore.feature.component";
 
 const ExploreComponent = () => {
   return (
-    <div className="pt-3">
+    <div className="pt-0">
       <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-3">
-        <Link to="/">
-          <div className="mb-4">
-            <BackHomeComponent />
+        <div className="pt-2 pb-6 flex gap-8">
+          <div className="w-64">
+            <Link to="/">
+              <div className="!rounded-button bg-gradient-to-r from-white/20 to-white/10 hover:from-white/30 hover:to-white/20 text-gray-300 px-3 py-2 flex items-center gap-2 transition-all duration-300 rounded">
+                <i className="fa-solid fa-left-long"></i> BACK
+              </div>
+            </Link>
           </div>
-        </Link>
+          <div className="flex-1">
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Search stories, genres, or authors..."
+                className="w-full pl-12 pr-4 py-2 text-base text-gray-300 bg-blue-500/10 border outline-1 -outline-offset-1 outline-gray-300 border-gray-300 rounded-md focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              />
+              <i className="fas fa-search absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400"></i>
+            </div>
+          </div>
+        </div>
+
         <div className="flex gap-8">
           <div className="w-64 flex-shrink-0">
             <div className="sticky top-1">
