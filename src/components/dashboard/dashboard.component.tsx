66cf0e8f54f23553ac610d7ae645a0005ea284af
Roni Sarkar <ronichandrasarkar@gmail.com>
2025-04-23 16:37:34 +0530

    changes

--- src/components/dashboard/dashboard.component.tsx
+++ src/components/dashboard/dashboard.component.tsx
@@ -14,73 +14,7 @@ const DashboardComponent = () => {
   }
   return (
     <div>
-      {/* <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div className="bg-white shadow-md rounded-2xl p-5">
-          <h2 className="text-xl font-semibold mb-3">Users</h2>
-          <div className="space-y-1 text-sm text-gray-700">
-            <p>
-              Total: <span className="font-medium">{data.users.total}</span>
-            </p>
-            <p>
-              Active: <span className="font-medium">{data.users.active}</span>
-            </p>
-            <p>
-              Inactive:{" "}
-              <span className="font-medium">{data.users.inactive}</span>
-            </p>
-            <p>
-              Blocked: <span className="font-medium">{data.users.blocked}</span>
-            </p>
-            <p>
-              Writers: <span className="font-medium">{data.users.writers}</span>
-            </p>
-            <p>
-              Apply for Writer:{" "}
-              <span className="font-medium">{data.users.applyForWriter}</span>
-            </p>
-          </div>
-        </div>
-
-        <div className="bg-white shadow-md rounded-2xl p-5">
-          <h2 className="text-xl font-semibold mb-3">Subscriptions</h2>
-          <div className="space-y-1 text-sm text-gray-700">
-            <p>
-              Free:{" "}
-              <span className="font-medium">{data.subscriptionTypes.free}</span>
-            </p>
-            <p>
-              Pro:{" "}
-              <span className="font-medium">{data.subscriptionTypes.pro}</span>
-            </p>
-            <p>
-              Premium:{" "}
-              <span className="font-medium">
-                {data.subscriptionTypes.premium}
-              </span>
-            </p>
-          </div>
-        </div>
-
-        <div className="bg-white shadow-md rounded-2xl p-5">
-          <h2 className="text-xl font-semibold mb-3">Posts</h2>
-          <div className="space-y-1 text-sm text-gray-700">
-            <p>
-              Total: <span className="font-medium">{data.posts.total}</span>
-            </p>
-            <p>
-              Published:{" "}
-              <span className="font-medium">{data.posts.published}</span>
-            </p>
-            <p>
-              Featured:{" "}
-              <span className="font-medium">{data.posts.featured}</span>
-            </p>
-          </div>
-        </div>
-      </div> */}
-
-      <DashboardAnalysisHeader data={data} />
-
+      {data && <DashboardAnalysisHeader data={data} />}
       {data && (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-4">
           <UsersPieChart data={data.users} />
